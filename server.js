cb9417151e4f6752ff3099d6717de99f80050e56
bguiz <bguiz@users.noreply.github.com>
2021-01-19 21:47:32 +0800

    feat: web3 and rns.js instances

--- server.js
+++ server.js
@@ -1,5 +1,33 @@
 const express = require('express');
 const vhost = require('vhost');
+const Web3 = require('web3');
+const RNS = require('@rsksmart/rns');
+
+const rpcUrl = process.env.RPC_URL ||
+  'https://public-node.rsk.co/';
+// Ref: https://developers.rsk.co/rif/rns/architecture/registry/
+const rnsRegistryAddress = process.env.RNS_REGISTRY_ADDRESS ||
+  '0xcb868aeabd31e2b66f74e9a55cf064abb31a4ad5';
+// Ref: https://developers.rsk.co/rif/rns/architecture/definitive-resolver/
+const rnsResolverAddress = process.env.RNS_RESOLVER_ADDRESS ||
+  '0xD87f8121D44F3717d4bAdC50b24E50044f86D64B';
+const rnsChainId = process.env.RNS_CHAIN_ID ||
+  31;
+
+// Ref: https://developers.rsk.co/rif/rns/libs/javascript/Advanced-usage/
+const web3 = new Web3(rpcUrl);
+const rns = new RNS(web3);
+// TODO figure out if custom initialisation is necessary
+// const rns = new RNS(
+//   web3,
+//   {
+//     contractAddresses: {
+//       // registry: rnsRegistryAddress.toLowerCase(),
+//       // resolver: rnsResolverAddress.toLowerCase(),
+//     },
+//     networkId: rnsChainId,
+//   }
+// );
 
 const server = express();
 
